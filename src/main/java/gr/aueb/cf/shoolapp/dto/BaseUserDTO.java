167f5b3bc04460bf8730687b79774d64d4d098f8
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-04-04 18:51:13 +0300

    school7-servlet-jsp-pro created

---
+++ src/main/java/gr/aueb/cf/shoolapp/dto/BaseUserDTO.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.shoolapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public abstract class BaseUserDTO {
+    private String username;
+    private String password;
+    private String confirmedPassword;
+
+
+    public BaseUserDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
