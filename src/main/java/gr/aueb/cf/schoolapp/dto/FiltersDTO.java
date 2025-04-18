9e9cb10269d602623a7b8e729350c21dc04fcb27
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-04-18 13:50:55 +0300

    DTO for filter teachers

---
+++ src/main/java/gr/aueb/cf/schoolapp/dto/FiltersDTO.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.schoolapp.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FiltersDTO {
+
+    private String firstname;
+    private String lastname;
+
+}
+
